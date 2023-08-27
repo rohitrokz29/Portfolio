87fa3727db37de9ae9ce6f074d768c49d7408add
Rohit Kharche <111701144+rohitrokz29@users.noreply.github.com>
2023-08-27 12:14:09 +0530

    Projects section
    Added Projects and fetched repos details using github api  and added elements to the page.

---
+++ js/projects.js
@@ -0,0 +1,36 @@
+const getProjects = async () => {
+    let result = await fetch('https://api.github.com/users/rohitrokz29/repos')
+        .then(res => res.json());
+    result.sort((a, b) => b.size - a.size)
+    console.log(result)
+    return result;
+}
+const createProjectList = async () => {
+    const projects = await getProjects();
+    let list = document.getElementById('project-list');
+
+    projects.map(item => {
+        let { name, description ,html_url} = item
+       if(name==="rohitrokz29"||name==="DSA"){
+        return;
+       }
+        let node = document.createElement('li');
+        node.classList.add("project-item");
+        node.style.backgroundImage=`url(../assets/${name}.png)`;
+        let projName=document.createElement('a');
+        projName.innerText=name;
+        projName.href=html_url;
+        projName.target="_blank";
+
+        let projDesc=document.createElement('div');
+        projDesc.appendChild(projName);
+        let desc=document.createElement('span');
+        desc.innerText=description?.length>100?description.slice(0,100)+'...':description;
+        projDesc.appendChild(desc);
+        
+        node.appendChild(projDesc);
+
+        list.appendChild(node);
+    });
+}
+createProjectList()
